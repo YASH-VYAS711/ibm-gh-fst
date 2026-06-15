cc275a2f917bad43f1c1c43167caec5cad94e108
YASH-VYAS711 <142643866+YASH-VYAS711@users.noreply.github.com>
2026-06-15 15:39:46 +0530

    Add files via upload

---
+++ java/Activity10/Activity10.java
@@ -0,0 +1,29 @@
+package Activity10;
+import java.util.HashSet;
+
+
+
+public class Activity10 {
+    public static void main(String[] args) {
+        HashSet<String> hs = new HashSet<String>();
+
+        hs.add("H");
+        hs.add("E");
+        hs.add("L");
+        hs.add("L");
+        hs.add("O");
+      
+        System.out.println("Original HashSet: " + hs); 
+        System.out.println("Size of HashSet: " + hs.size());
+        System.out.println("Removing A from HashSet: " + hs.remove("H"));
+
+        if(hs.remove("Z")) {
+        	System.out.println("Z removed from the Set");
+        } else {
+        	System.out.println("Z is not present in the Set");
+        }
+        
+        System.out.println("Checking if M is present: " + hs.contains("P"));
+        System.out.println("Updated HashSet: " + hs);
+    }
+}
