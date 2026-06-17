9c6d4b982546fc086cf1b584055d74d5d75937f0
YASH-VYAS711 <142643866+YASH-VYAS711@users.noreply.github.com>
2026-06-17 14:07:49 +0530

    Add files via upload

---
+++ junit/RPS/GameUiTest.java
@@ -0,0 +1,23 @@
+package RPS;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import RPS.GameUi;
+
+class GameUiTest {
+
+    @Test
+    void invalidUserChoiceTest() {
+
+        assertThrows(IllegalArgumentException.class,
+                () -> GameUi.showInput(5,0,"DRAW",0,0));
+    }
+
+    @Test
+    void invalidMachineChoiceTest() {
+
+        assertThrows(IllegalArgumentException.class,
+                () -> GameUi.showInput(0,5,"DRAW",0,0));
+    }
+}
