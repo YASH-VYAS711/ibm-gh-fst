42d93237f3df7d2e2e8774e904141a7e8e697b8b
YASH-VYAS711 <142643866+YASH-VYAS711@users.noreply.github.com>
2026-06-23 10:41:18 +0530

    Add files via upload

---
+++ selenium/Activity10.java
@@ -0,0 +1,40 @@
+package Activity;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Activity10 {
+
+	public static void main(String[] args) {
+	    WebDriver driver = new FirefoxDriver();
+	    Actions builder = new Actions(driver);
+	    // Open the browser
+	    driver.get("https://training-support.net/webelements/mouse-events");
+
+	    // Perform testing and assertions
+	    System.out.println("Title of the page: "+driver.getTitle());
+	    WebElement text = driver.findElement(By.id("result"));
+	    WebElement clickButton = driver.findElement(By.xpath("//h1[contains(text(),'Cargo.lock')]"));
+	    WebElement tomlButton = driver.findElement(By.xpath("//h1[contains(text(),'Cargo.toml')]"));
+	    WebElement srcButton = driver.findElement(By.xpath("//h1[contains(text(),'src')]"));
+	    clickButton.click();
+	    System.out.println("Text: "+text.getText());
+	    builder.moveToElement(tomlButton).build().perform();
+	    tomlButton.click();
+	    System.out.println("Text: "+text.getText());
+	    builder.moveToElement(srcButton).build().perform(); 
+	    builder.doubleClick(srcButton).build().perform();// Close the browser
+	    System.out.println("Text: "+text.getText());
+	    
+	    // Feel free to comment out the line below
+	    // so it doesn't close too quickly
+	    driver.quit();
+	  }
+	}
